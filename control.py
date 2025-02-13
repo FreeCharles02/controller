913920997c1987cd067d681455abf548b6988670
Rocyon <161540596+rocyon@users.noreply.github.com>
2025-02-13 16:00:16 -0600

    Extended Deadzone

--- control.py
+++ control.py
@@ -10,48 +10,56 @@ Button = "Button"
 Axis = "Axis"
 Hat = "Hat"
 
-LeftBumper = "LB";  RightBumper = "RB"
-LeftTrigger = "LT"; RightTrigger = "RT"
+LeftBumper = "LB"
+RightBumper = "RB"
+LeftTrigger = "LT"
+RightTrigger = "RT"
 
-AButton = "A"; BButton = "B"
-XButton = "X"; YButton = "Y"
+AButton = "A"
+BButton = "B"
+XButton = "X"
+YButton = "Y"
 HomeButton = "HOME"
 
-LeftJoyLeftRight = "LJ_LR"; LeftJoyUpDown = "LJ_UD"
+LeftJoyLeftRight = "LJ_LR"
+LeftJoyUpDown = "LJ_UD"
 LeftJoyIn = "LJ_IN"
-RightJoyLeftRight = "RJ_LR";RightJoyUpDown = "RJ_UD"
+RightJoyLeftRight = "RJ_LR"
+RightJoyUpDown = "RJ_UD"
 RightJoyIn = "RJ_IN"
 
-DpadLeftRight = "D_LR"; DpadUpDown = "D_UD"
+DpadLeftRight = "D_LR"
+DpadUpDown = "D_UD"
 
 ControllerMappings = {
-    "Nintendo Switch Pro Controller" : {
-        AButton : (Button, 1), BButton : (Button, 0),
-        XButton : (Button, 2), YButton : (Button, 3),
-        LeftBumper : (Button, 5),  RightBumper : (Button, 6),
-        LeftTrigger : (Button, 7), RightTrigger : (Button, 8),
-        LeftJoyIn : (Button, 12),  RightJoyIn : (Button, 13),
-        HomeButton : (Button, 11),
+    "Nintendo Switch Pro Controller": {
+        AButton: (Button, 1), BButton: (Button, 0),
+        XButton: (Button, 2), YButton: (Button, 3),
+        LeftBumper: (Button, 5),  RightBumper: (Button, 6),
+        LeftTrigger: (Button, 7), RightTrigger: (Button, 8),
+        LeftJoyIn: (Button, 12),  RightJoyIn: (Button, 13),
+        HomeButton: (Button, 11),
 
-        LeftJoyLeftRight : (Axis, 0, 1),  LeftJoyUpDown : (Axis, 1, -1),
-        RightJoyLeftRight : (Axis, 2, 1), RightJoyUpDown : (Axis, 3, -1),
+        LeftJoyLeftRight: (Axis, 0, 1),  LeftJoyUpDown: (Axis, 1, -1),
+        RightJoyLeftRight: (Axis, 2, 1), RightJoyUpDown: (Axis, 3, -1),
 
-        DpadLeftRight : (Hat, 0, 0), DpadUpDown : (Hat, 0, 1),
+        DpadLeftRight: (Hat, 0, 0), DpadUpDown: (Hat, 0, 1),
     },
     "Xbox One S Controller": {
-        AButton : (Button, 0), BButton : (Button, 1),
-        XButton : (Button, 3), YButton : (Button, 4),
-        LeftBumper : (Button, 6),  RightBumper : (Button, 7),
-        LeftTrigger : (Axis, 4, 1), RightTrigger : (Axis, 5, 1),
-        LeftJoyIn : (Button, 13),  RightJoyIn : (Button, 14),
-        HomeButton : (Button, 12),
+        AButton: (Button, 0), BButton: (Button, 1),
+        XButton: (Button, 3), YButton: (Button, 4),
+        LeftBumper: (Button, 6),  RightBumper: (Button, 7),
+        LeftTrigger: (Axis, 4, 1), RightTrigger: (Axis, 5, 1),
+        LeftJoyIn: (Button, 13),  RightJoyIn: (Button, 14),
+        HomeButton: (Button, 12),
 
-        LeftJoyLeftRight : (Axis, 0, 1),  LeftJoyUpDown : (Axis, 1, -1),
-        RightJoyLeftRight : (Axis, 2, 1), RightJoyUpDown : (Axis, 3, -1),
+        LeftJoyLeftRight: (Axis, 0, 1),  LeftJoyUpDown: (Axis, 1, -1),
+        RightJoyLeftRight: (Axis, 2, 1), RightJoyUpDown: (Axis, 3, -1),
 
-        DpadLeftRight : (Hat, 0, 0), DpadUpDown : (Hat, 0, 1),
+        DpadLeftRight: (Hat, 0, 0), DpadUpDown: (Hat, 0, 1),
     }
 }
+
 
 def pollJoy(joystick, input_source):
     try:
@@ -75,19 +83,20 @@ def pollJoy(joystick, input_source):
     print(f"Unable to find source \"{source}\".")
     exit(1)
 
+
 def calculateMecanumWheel(joystick, deadzone):
     speed  = pollJoy(joystick, LeftJoyUpDown)
     strafe = pollJoy(joystick, LeftJoyLeftRight)
     turn   = pollJoy(joystick, RightJoyLeftRight)
 
     deadzone = abs(deadzone)
-    if speed>-deadzone and speed<deadzone:
+    if speed > -deadzone and speed < deadzone:
         speed = 0
 
-    if strafe>-deadzone and strafe<deadzone:
+    if strafe > -deadzone and strafe < deadzone:
         strafe = 0
 
-    if turn>-deadzone and turn<deadzone:
+    if turn > -deadzone and turn < deadzone:
         turn = 0
 
     # Map joysticks onto mecanum wheels
@@ -97,7 +106,7 @@ def calculateMecanumWheel(joystick, deadzone):
     rBwd = speed + strafe - turn
 
     # Calculate if any values exceed 1
-    peak = max(abs(lFwd),abs(lBwd),abs(rFwd),abs(rBwd),1)
+    peak = max(abs(lFwd), abs(lBwd), abs(rFwd), abs(rBwd), 1)
 
     # divide by that value so no value exceeds 1
     lFwd /= peak
@@ -107,10 +116,11 @@ def calculateMecanumWheel(joystick, deadzone):
 
     return (lFwd, lBwd, rFwd, rBwd)
 
+
 def main():
     clock = pygame.time.Clock()
     joysticks = {}
-    ser = serial.Serial('/dev/ttyACM0', 9600);
+    ser = serial.Serial('/dev/ttyACM0', 9600)
 
     while True:
         for event in pygame.event.get():
@@ -126,26 +136,24 @@ def main():
                 del joysticks[event.instance_id]
                 print(f"{joy.get_name()}, id#{event.instance_id} disconnected")
 
-        lf,lb,rf,rb = 0,0,0,0
+        lf, lb, rf, rb = 0, 0, 0, 0
         for joystick in joysticks.values():
-            lf,lb,rf,rb = calculateMecanumWheel(joystick, 0.08);
-
+            lf, lb, rf, rb = calculateMecanumWheel(joystick, 0.15)
             lf = int(lf*63)
             lb = int(lb*63)
             rf = int(rf*63)
             rb = int(rb*63)
 
-            newestMotorOut = (lf,lb,rf,rb)
-        
-        spd = 124
-        ser.write(struct.pack('!bbbb',lf,lb,rf,rb))
+        ser.write(struct.pack('!bbbb', lf, lb, rf, rb))
         os.system('clear')
+        print(f"{len(joysticks.values())}\n{lf} | {rf}\n{lb} | {rb}")
         print("{")
-        while ser.in_waiting>0:
+        while ser.in_waiting > 0:
             print(f"\tA: {ser.read()}\n\tB: {ser.read()}\n\tC: {ser.read()}\n\tD: {ser.read()}\n")
             ser.readline()
         print("}")
         clock.tick(30)
+
 
 if __name__ == "__main__":
     main()
