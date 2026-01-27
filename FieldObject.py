88e5c85b36587c959a66fd33baf3f57d143fa0ec
Charles <105826694+FreeCharles02@users.noreply.github.com>
2026-01-26 21:40:52 -0600

    april tag id

--- FieldObject.py
+++ FieldObject.py
@@ -5,7 +5,16 @@ class FieldObject:
     def __init__(self, distance_sense, box_name):
        self.distance = distance_sense
        self.name = box_name
+       self.april_tag_id
 
+    def __init__(self, distance_sense, box_name, april_tag_id):
+        self.distance = distance_sense
+        self.name = box_name
+        self.april_tag_id = april_tag_id
+    
     #We capture and set the changing disance as the distance decreaes or increases. 
     def set_distance(self,distance_sense):
         self.distance = distance_sense
+    
+    def set_april_tag(self, id):
+        self.april_tag_id = id
